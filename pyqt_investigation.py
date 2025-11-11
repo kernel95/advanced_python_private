b24eb4764ca69751588379764f36372aa34b39f3
kernel95 <kelmek95@gmail.com>
2025-11-11 12:33:10 +0000

    pyqt done

--- pyqt_investigation.py
+++ pyqt_investigation.py
@@ -1,7 +1,8 @@
-# pyqt_investigation.py
+# pyqt6_investigation.py
 
 import sys
 from PyQt6.QtWidgets import QWidget, QApplication, QPushButton, QMessageBox, QTableWidget, QTableWidgetItem
+
 from member_dao import MemberDao
 
 class App(QWidget):
@@ -9,29 +10,29 @@ class App(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("PyQt6 Sample")
-        self.setGeometry(100,100,600,400)
-
+        self.setGeometry(100, 100, 600, 400)
+        
         self.btn_test = QPushButton("Press Me", self)
-        self.btn_test.move(10,5)
+        self.btn_test.move(10, 5)
         self.btn_test.clicked.connect(self.btn_test_clicked)
-        #let's import the members
+
         self.dao = MemberDao()
         self.members = self.dao.get_all()
-        #print (self.members) #just check you got it
 
-        #let's use the db table and put it in the gui
         self.member_table = QTableWidget(self)
-        self.member_table.setGeometry(10,30,580,370) #0,0 means top left
+        self.member_table.setGeometry(10, 30, 580,360)
         self.member_table.setColumnCount(4)
         self.member_table.setHorizontalHeaderLabels(["ID", "Name", "Email", "Active"])
 
-        self.member_table.setColumnWidth(0, 50)
+        self.member_table.setColumnWidth(0, 90)
         self.member_table.setColumnWidth(1, 200)
         self.member_table.setColumnWidth(2, 200)
-        self.member_table.setColumnWidth(3, 90)
+        self.member_table.setColumnWidth(3, 50)
 
-        #add them in the gui
+        self.member_table.itemChanged.connect(self.member_table_item_changed)
+
         self.member_table.setRowCount(len(self.members))
+
         for row, member in enumerate(self.members):
             self.member_table.setItem(row, 0, QTableWidgetItem(str(member.id)))
             self.member_table.setItem(row, 1, QTableWidgetItem(member.name))
@@ -39,16 +40,53 @@ class App(QWidget):
             self.member_table.setItem(row, 3, QTableWidgetItem("Yes" if member.active else "No"))
 
 
+        self.btn_delete = QPushButton("Delete", self)
+        self.btn_delete.move(50, 5)
+        self.btn_delete.clicked.connect(self.btn_delete_clicked)
+
+    def member_table_item_changed(self, item):
+
+        row = item.row()
+        column = item.column()
+
+        id = int(self.member_table.item(row, 0).text())
+
+        member = [member for member in self.members if member.id == id][0]
+        
+        if column == 1:
+            member.name = item.text()
+        
+        self.dao.update(member)
+
+
+
+
+
+    def btn_delete_clicked(self):
+        answer = QMessageBox.question(self, 
+                            "Confirm Delete", 
+                            "Are you sure?", 
+                            QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No, 
+                            QMessageBox.StandardButton.No)
+        
+        if answer == QMessageBox.StandardButton.Yes :
+            selected_items = self.member_table.selectedItems()
+            row = selected_items[0].row()
+            id = self.member_table.item(row, 0).text()
+
+            if self.dao.delete(int(id)) > 0:
+                self.member_table.removeRow(row)
+
+
+
+
 
     def btn_test_clicked(self):
-        QMessageBox.information(self, "Click", "You clicked the button") #debugging 
-
+        QMessageBox.information(self, "Click", "You clicked the button")
 
     def closeEvent(self, event):
-        #QMessageBox.information(self, "Closing db", "The application is closing")
+        # QMessageBox.information(self, "Closing", "The application is closing")
         self.dao.close()
-        #return super().closeEvent(event)
-
 
 if __name__ == "__main__":
 
